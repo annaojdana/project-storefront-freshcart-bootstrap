48aa7e8be985d09091a0cb0cb8554f905a113df2
Anna <anna.ojdana@gmail.com>
2023-01-17 17:39:46 +0100

    strore, home, category route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryProductsComponent } from './components/category-products/category-products.component';
+import { StoreProductsComponent } from './components/store-products/store-products.component';
+import { HomeComponent } from './components/home/home.component';
+import { CategoryProductsComponentModule } from './components/category-products/category-products.component-module';
+import { StoreProductsComponentModule } from './components/store-products/store-products.component-module';
+import { HomeComponentModule } from './components/home/home.component-module';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'categories/:categoryId', component: CategoryProductsComponent }, { path: 'stores/:storeId', component: StoreProductsComponent }, { path: '', component: HomeComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), CategoryProductsComponentModule, StoreProductsComponentModule, HomeComponentModule],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
