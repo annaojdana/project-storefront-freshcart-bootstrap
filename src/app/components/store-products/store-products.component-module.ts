8f9ae2480606fc6c4ab7498d8f63911a0fe7c627
Anna <anna.ojdana@gmail.com>
2023-01-25 10:58:40 +0100

    search by product implemented

--- src/app/components/store-products/store-products.component-module.ts
+++ src/app/components/store-products/store-products.component-module.ts
@@ -1,9 +1,10 @@
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreProductsComponent } from './store-products.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   declarations: [StoreProductsComponent],
   providers: [],
   exports: [StoreProductsComponent]
--- src/app/components/store-products/store-products.component.ts
+++ src/app/components/store-products/store-products.component.ts
@@ -5,12 +5,20 @@ import {
 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, combineLatest } from 'rxjs';
-import { filter, map, switchMap, tap } from 'rxjs/operators';
+import {
+  debounceTime,
+  map,
+  shareReplay,
+  startWith,
+  switchMap,
+  tap,
+} from 'rxjs/operators';
 import { StoreQueryModel } from '../../query-models/store.query-model';
 import { StoresService } from '../../services/stores.service';
 import { ProductsService } from '../../services/products.service';
 import { ProductModel } from '../../models/product.model';
 import { StoreModel } from '../../models/store.model';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-store-products',
@@ -20,15 +28,29 @@ import { StoreModel } from '../../models/store.model';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StoreProductsComponent {
-  
+  readonly searchForm: FormGroup = new FormGroup({
+    search: new FormControl(),
+  });
+  private readonly _searchValue$: Observable<string> =
+    this.searchForm.valueChanges.pipe(
+      map((form) => form.search),
+      debounceTime(1000),
+      startWith(''),
+      shareReplay(1)
+    );
+
   readonly storeWithProducts$: Observable<StoreQueryModel> = combineLatest([
     this._activatedRoute.params.pipe(
       switchMap((data) => this._storesService.getOneStore(data['storeId']))
     ),
     this._productsService.getAll(),
+    this._searchValue$,
   ]).pipe(
-    map(([store, products]) => this._mapToStoreWithProducts(products, store)),
-    tap(console.log)
+    map(([store, products, search]) =>
+      this._mapToSearchProducts(products, store, search)
+    ),
+    tap(console.log),
+    shareReplay(1)
   );
 
   constructor(
@@ -37,16 +59,19 @@ export class StoreProductsComponent {
     private _productsService: ProductsService
   ) {}
 
-  private _mapToStoreWithProducts(
+  private _mapToSearchProducts(
     products: ProductModel[],
-    store: StoreModel
+    store: StoreModel,
+    search: string
   ): StoreQueryModel {
     return {
       name: store.name,
       logoUrl: store.logoUrl,
       id: store.id,
       distanceInKm: store.distanceInMeters / 1000,
-      products: products.filter((p) => p.storeIds.includes(store.id)),
+      products: products
+        .filter((p) => p.storeIds.includes(store.id))
+        .filter((p) => p.name.toLowerCase().includes(search.toLowerCase())),
     };
   }
 }
